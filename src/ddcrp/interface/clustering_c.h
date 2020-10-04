06b2153d9a207d116b29a4019a52880664a40fc1
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-10-04 19:26:42 +0800

    remove old c ver

--- src/ddcrp/interface/clustering_c.h
+++ src/ddcrp/interface/clustering_c.h
@@ -22,20 +22,6 @@ extern "C" {
             const float64* adj_logdecay, //
             uint64* cluster_assignment // output assignment: preallocated
     );
-
-
-    void clustering_c(
-            uint64 seed, // random seed
-            uint64 num_iterations, // num_iterations
-            uint64 num_nodes, uint64 dimension, // num_nodes, embedding dimension
-            const float64* embedding, // (d x n) col major matrix
-            float64 logalpha, // logalpha
-            uint64 num_edges, // num edges
-            const uint64* adj_row, // coo_matrix for logdecay
-            const uint64* adj_col, //
-            const float64* adj_logdecay, //
-            uint64* cluster_assignment // output assignment: preallocated
-    );
 };
 
 
