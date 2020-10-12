9340b1b88590870d24680b9f04ca6ca0faedf799
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-10-13 01:46:44 +0800

    refactor

--- src/ddcrp/core/ddcrp.h
+++ src/ddcrp/core/ddcrp.h
@@ -13,49 +13,8 @@
 #include "common.h"
 #include "math.h"
 #include "ds.h"
+#include "assignment.h"
 
-using Customer = uint64;
-using Table = uint64;
-
-const Customer customer_nil = uint64_nil;
-const Table table_nil = uint64_nil;
-
-class Assignment {
-public:
-    explicit Assignment(Customer num_customers);
-
-    ~Assignment() = default;
-
-    void unlink(Customer source);
-
-    void link(Customer source, Customer target);
-
-    [[nodiscard]] Customer num_customers() const;
-
-    [[nodiscard]] Table table(Customer customer) const;
-
-    [[nodiscard]] std::vector<Customer> component(Customer customer) const;
-
-    [[nodiscard]] std::vector<Table> table_assignment() const;
-
-private:
-    struct Node {
-        Customer m_parent;
-        ds::small_set<Customer, customer_nil> m_children;
-
-        Node();
-
-        ~Node() = default;
-    };
-
-    uint64 m_num_customers;
-    std::vector<Node> m_adjacency_list;
-    std::vector<Table> m_table_assignment;
-    uint64 m_table_count;
-
-    std::set<Customer> weakly_connected_component(Customer customer);
-
-};
 //template<typename UnitRNG>
 void ddcrp_iterate(
         std::function<float64()> gen,
@@ -65,109 +24,6 @@ void ddcrp_iterate(
                 Customer)> &logdecay_func, // logdecay = logdecay_func[customer1][customer2]
         const std::function<float64(const std::vector<Customer> &)> &loglikelihood_func // loglikelihood of a compoentn
 );
-
-Assignment::Node::Node() :
-        m_parent(customer_nil), m_children() {}
-
-Assignment::Assignment(Customer num_customers) :
-//init default, each customer sits in one table
-//:param num_customers: number of customers
-        m_num_customers(num_customers),
-        m_adjacency_list(),
-        m_table_assignment(),
-        m_table_count(num_customers) {
-    for (Customer customer = 0; customer < num_customers; customer++) {
-        m_adjacency_list.emplace_back();
-        m_table_assignment.push_back(customer);
-    }
-}
-
-std::set<Customer> Assignment::weakly_connected_component(Customer customer) {
-    std::set<Customer> visited;
-    std::vector<Customer> frontier({customer});
-
-    auto is_in_list = [](const std::vector<Customer> &list, Customer customer) -> bool {
-        for (Customer item: list) {
-            if (item == customer) {
-                return true;
-            }
-        }
-        return false;
-    };
-    while (not frontier.empty()) {
-        auto current = frontier.back();
-        frontier.pop_back();
-        visited.insert(current);
-        auto node = m_adjacency_list[current];
-        auto adding = node.m_children;
-        adding.insert(node.m_parent);
-        adding.foreach([&](Customer new_customer) {
-            if (new_customer != customer_nil and not is_in_list(frontier, new_customer) and
-                not (visited.find(new_customer) != visited.end())) {
-                frontier.push_back(new_customer);
-            }
-        });
-    }
-    return visited;
-}
-
-void Assignment::unlink(Customer source) {
-    auto target = m_adjacency_list[source].m_parent;
-    if (target == customer_nil) {
-        return;
-    }
-    // remove link
-
-    m_adjacency_list[target].m_children.erase(source);
-    m_adjacency_list[source].m_parent = customer_nil;
-    // update assingment
-    auto new_source_component = weakly_connected_component(source);
-    auto new_source_table = m_table_count;
-    m_table_count++;
-    for (auto new_source_customer: new_source_component) {
-        m_table_assignment[new_source_customer] = new_source_table;
-    }
-}
-
-void Assignment::link(Customer source, Customer target) {
-    // add link
-    m_adjacency_list[source].m_parent = target;
-    m_adjacency_list[target].m_children.insert(source);
-    if (m_table_assignment[source] != m_table_assignment[target]) {
-        // update assingment
-        auto new_join_table = m_table_count;
-        m_table_count += 1;
-        auto new_join_component = weakly_connected_component(source);
-        for (auto new_join_customer: new_join_component) {
-            m_table_assignment[new_join_customer] = new_join_table;
-        }
-    }
-}
-
-Customer Assignment::num_customers() const {
-    return m_num_customers;
-}
-
-Table Assignment::table(Customer customer) const {
-    return m_table_assignment[customer];
-}
-
-std::vector<Customer> Assignment::component(Customer customer) const {
-    auto table = m_table_assignment[customer];
-    auto component = std::vector<Customer>();
-    component.reserve(m_num_customers);
-    for (Customer c_customer=0; c_customer < m_num_customers; c_customer++) {
-        if (m_table_assignment[c_customer] == table) {
-            component.push_back(c_customer);
-        }
-    }
-    return component;
-}
-
-std::vector<Table> Assignment::table_assignment() const {
-    return std::vector<Table>(m_table_assignment);
-}
-
 
 void ddcrp_iterate(
         std::function<float64()> gen,
@@ -236,6 +92,5 @@ void ddcrp_iterate(
         assignment.link(source, target);
     }
 }
-
 
 #endif //DDCRP_DDCRP_H
