49a0b51d3c119c067ac0ac42a44ada7ff7601463
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-10-13 16:20:08 +0800

    fixed wlak

--- src/deepwalk/walk.py
+++ src/deepwalk/walk.py
@@ -8,7 +8,10 @@ def random_walk(g: nx.Graph, walks_per_node: int = 1, walk_length: int = 1) -> L
         for _ in range(walks_per_node):
             path: List[int] = [start,]
             for _ in range(walk_length):
-                next = random.choice(list(g.neighbors(path[-1])))
+                neighbors = list(g.neighbors(path[-1]))
+                if len(neighbors) == 0:
+                    break
+                next = random.choice(neighbors)
                 path.append(next)
             walks.append(path)
     return walks
