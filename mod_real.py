cf8daf7cffb87996f3d7d3b684b722e1525100ee
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-10-14 20:45:32 +0800

    refactor

--- mod_real.py
+++ mod_real.py
@@ -19,7 +19,7 @@ edge_list.sort(key= lambda x: timestamp(x))
 dim = 50
 num_folds = 1000
 fold_size = int(len(edge_list) / num_folds)
-for window in range(10, 101, 10):
+for window in range(10, 51, 10):
      for scale in range(1000, 10001, 1000):
           start = 0
           model = Model(seed, mg.number_of_nodes(), dim)
@@ -32,10 +32,12 @@ for window in range(10, 101, 10):
                     timestamp(edge_list[end]),
                )
                embedding = model.deepwalk_embedding(g)
-               comm, kmeans_improved_comm, kmeans_comm = Model(seed, g.number_of_nodes(), dim).ddcrp_iterate(g, embedding, ddcrp_scale=scale)
+               hop = 2
+               comm, kmeans_improved_comm, kmeans_comm = model.ddcrp_iterate(g, embedding, ddcrp_scale=scale, receptive_hop=hop)
                log.write_log(f"range: {start} -> {end}")
                log.write_log(f"window: {window}")
                log.write_log(f"scale {scale}")
+               log.write_log(f"hop {hop}")
                log.write_log(f"cluster size {len(kmeans_improved_comm)} kmeans improved modularity: {nx.algorithms.community.quality.modularity(g, kmeans_improved_comm)}")
                log.write_log(f"cluster size {len(kmeans_comm)} kmeans naive    modularity: {nx.algorithms.community.quality.modularity(g, kmeans_comm)}")
 
