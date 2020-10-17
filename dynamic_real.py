9180003140fffbb34c15988135c257a02554ac00
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-10-17 20:24:58 +0800

    fixed

--- dynamic_real.py
+++ dynamic_real.py
@@ -1,3 +1,4 @@
+import time
 from typing import List, Tuple, Dict, Any, Set
 
 import networkx as nx
@@ -22,9 +23,27 @@ edge_list.sort(key=lambda x: timestamp(x))
 dim = 50
 num_folds = 1000
 fold_size = int(len(edge_list) / num_folds)
+ddcrp_cutoff = 5
+
+
+def log_filename(hop: int, window: int, scale: float) -> str:
+    return f"hop_{hop}_window_{window}_scale_{scale}"
+
+def write_first_line(hop: int, window: int, scale: float):
+    log.write_csv(
+        data=["from_timestamp", "to_timestamp", "average degree", "predicted cluster size", "modularity", "performance", "improved modularity", "improved performance", "naive modularity", "naive performance", "ddcrp time", "response"],
+        name=log_filename(hop, window, scale),
+    )
+def write_line(hop: int, window: int, scale: float, from_timestamp: int, to_timestamp: int, average_degree: float, predicted_cluster_size: int, modularity: float, performance: float, improved_modularity: float, improved_performance: float, naive_modularity: float, naive_performance: float, ddcrp_time: float, response: Any):
+    log.write_csv(
+        data=[from_timestamp, to_timestamp, average_degree, predicted_cluster_size, modularity, performance, improved_modularity, improved_performance, naive_modularity, naive_performance, ddcrp_time, response],
+        name=log_filename(hop, window, scale),
+    )
+
 for hop in [1, 2]:
     for window in range(10, 51, 10):
         for scale in range(1000, 10001, 1000):
+            write_first_line(hop, window, scale)
             start = 0
             model = Model(seed, mg.number_of_nodes(), dim)
             end_loop = False
@@ -37,48 +56,56 @@ for hop in [1, 2]:
                     end = len(edge_list) - 1
                     end_loop = True
                 #####
+                from_timestamp = timestamp(edge_list[start])
+                to_timestamp = timestamp(edge_list[end])
                 g = subgraph_by_timestamp(
                     mg,
-                    timestamp(edge_list[start]),
-                    timestamp(edge_list[end]),
+                    from_timestamp,
+                    to_timestamp,
                 )
+                average_degree = 2 * g.number_of_edges() / g.number_of_nodes()
                 embedding = model.deepwalk(g)
+                t0 = time.time()
                 comm_list = model.ddcrp(g, embedding, ddcrp_scale=scale, receptive_hop=hop)
-                naive_comm, mapping = model.mcla(comm_list[5:], comm)
-                new_comm = model.kmeans(embedding, naive_comm)
-                def report(new_comm: List[Set[int]]):
+                ddcrp_time = time.time() - t0
+                comm_list = comm_list[ddcrp_cutoff:]
+                init_comm, mapping = model.mcla(comm_list[5:], comm)
+                predicted_cluster_size = len(init_comm)
+                new_comm = model.kmeans(embedding, init_comm)
+                def response(new_comm: List[Set[int]]) -> Any:
+                    out = []
                     for i, c in enumerate(mapping):
                         if len(c) == 0:
-                            log.write_log(f"new comm")
-                            join = new_comm[i]
-                            log.write_log(f"\tjoin:  {len(join)} {join}")
+                            out.append({
+                                "type": "new",
+                                "join": list(new_comm[i]),
+                            })
                         elif len(c) == 1:
-                            log.write_log(f"old comm:")
-                            intersection = set_intersection([comm[i], new_comm[i]])
-                            leave = set_difference(comm[i], new_comm[i])
-                            join =  set_difference(new_comm[i], comm[i])
-                            log.write_log(f"\tinter: {len(intersection)} {intersection}")
-                            log.write_log(f"\tleave: {len(leave)} {leave}")
-                            log.write_log(f"\tjoin:  {len(join)} {join}")
+                            out.append({
+                                "type": "old",
+                                "remain": list(set_intersection([comm[i], new_comm[i]])),
+                                "leave": list(set_difference(comm[i], new_comm[i])),
+                                "join": list(set_difference(new_comm[i], comm[i])),
+                            })
                         else:
-                            new = new_comm[i]
-                            log.write_log(f"join comm:")
-                            intersection =set_intersection([set_union([comm[i] for i in c]), new_comm[i]])
-                            leave =  set_difference(set_union([comm[i] for i in c]), new_comm[i])
-                            join = set_difference(new_comm[i], set_union([comm[i] for i in c]))
-                            log.write_log(f"\tinter: {len(intersection)} {intersection}")
-                            log.write_log(f"\tleave: {len(leave)} {leave}")
-                            log.write_log(f"\tjoin:  {len(join)} {join}")
-                report(new_comm)
+                            out.append({
+                                "type": "join",
+                                "remain": list(set_intersection([set_union([comm[i] for i in c]), new_comm[i]])),
+                                "leave": list(set_difference(set_union([comm[i] for i in c]), new_comm[i])),
+                                "join": list(set_difference(new_comm[i], set_union([comm[i] for i in c]))),
+                            })
+                    return out
 
-                log.write_log(f"range: {start} -> {end}")
-                log.write_log(f"window: {window}")
-                log.write_log(f"scale {scale}")
-                log.write_log(f"hop {hop}")
-                log.write_log(f"{mapping}")
-                log.write_log(f"modularity {nx.algorithms.community.quality.modularity(g, naive_comm)}")
-                log.write_log(f"modularity {nx.algorithms.community.quality.modularity(g, new_comm)}")
-                log.write_log(f"modularity {nx.algorithms.community.quality.modularity(g, model.kmeans(embedding, len(naive_comm)))}")
+                modularity = nx.algorithms.community.quality.modularity(g, init_comm)
+                performance = nx.algorithms.community.quality.performance(g, init_comm)
+                improved_modularity = nx.algorithms.community.quality.modularity(g, new_comm)
+                improved_performance = nx.algorithms.community.quality.performance(g, new_comm)
+
+                naive_comm = model.kmeans(embedding, len(init_comm))
+                naive_modularity = nx.algorithms.community.quality.modularity(g, naive_comm)
+                naive_performance = nx.algorithms.community.quality.performance(g, naive_comm)
+
+                write_line(hop, window, scale, from_timestamp, to_timestamp, average_degree, predicted_cluster_size, modularity, performance, improved_modularity, improved_performance, naive_modularity, naive_performance, ddcrp_time, response(new_comm))
 
                 #####
                 start += fold_size
